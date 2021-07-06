e1b4fb1ff479d31e1d3ce5806e4f38023bf71a2c
nick-echevarria <nick.echev@gmail.com>
2021-07-06 21:05:06 +0200

    adding files for gh pages

--- src/App.js
+++ src/App.js
@@ -13,12 +13,18 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+  
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
+  }
+  // Arrow functions are bound to scope they are defined so 
+  // they're used for class methods
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
   }
 
   render() {
@@ -31,11 +37,12 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         {/* if we want to have some action run directly after a setState, we'll need to pass a callback
         directly after in order to have it run then and there */}
         <SearchBox
           placeholder="search monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
